4d517642f5931b1fc0296431006db9b281b487a2
Chella Varshitha <varshitha1436@gmail.com>
2026-05-06 09:59:46 +0530

    code changed

--- IND_TEL/views.py
+++ IND_TEL/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hyderabad(request):
-    return render(request,'hyd.html')
+    places=["Golkonda","sanghi","charminar","chilkur","Ramoji Film City"
+        
+    ]
+    context={"places":places}
+
+    return render(request,'hyd.html',context)
 
 def warangal(request):
     response="<h1>welcome to warangal</h1>"
